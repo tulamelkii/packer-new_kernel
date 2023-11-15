42da90e70ba626dc3829c3ecbe9e711c41778ec4
localadm <tulamelkii@yandex.ru>
2023-11-15 13:55:27 +0300

    new_box_debian

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEBIAN_FRONTEND=noninteractive apt update -y
+DEBIAN_FRONTEND=noninteractive apt upgrade -y
 DEBIAN_FRONTEND=noninteractive apt clean all
 
 
@@ -9,21 +10,5 @@ sudo mkdir -pm 700 /home/vagrant/.ssh
 sudo curl -sL https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
 sudo chmod 0600 /home/vagrant/.ssh/authorized_keys
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh
-
-
-# Удаление временных файлов
-rm -rf /tmp/*
-sudo rm  -f /var/log/auth*
-sudo rm  -f /var/log/boot*
-sudo rm  -f /var/log/btmp*
-sudo rm  -f /var/log/kern*
-sudo rm  -f /var/log/sys*
-
-rm -rf /var/cache/* /usr/share/doc/*
-sudo rm -rf /var/cache/apt
-rm -rf /vagrant/home/*.iso
-rm  -f ~/.bash_history
-history -c
-
-rm -rf /run/log/journal/*
+#sudo echo 'if [ -f ~/.bashrc ]; then source ~/.bashrc; fi' >> ~/.bash_profile
 
